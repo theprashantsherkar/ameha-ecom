afa409cb505b999ce28a61a399187a0dc2664a6a
Prashant <prashantsherkar7515400@gmail.com>
2025-08-01 23:40:58 +0530

    edited viteconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
 
+if (!globalThis.crypto) {
+  globalThis.crypto = require('crypto').webcrypto;
+}
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
